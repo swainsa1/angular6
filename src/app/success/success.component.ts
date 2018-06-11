7ded055ceda3b43dbd10b8c7256ea8fca72e9cc0
uswaisa <santosh.swain@pearson.com>
2018-06-11 17:55:36 -0500

    adding assignment1

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: '[app-success]',
+    templateUrl: './success.component.html',
+    styles: [`
+       h3 {
+            color:green
+          }
+          `]
+})
+export class SuccessComponent {
+
+}
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: '.app-warning',
+    templateUrl: './warning.component.html',
+    styles: [` h3 {
+                  color: pink
+               }
+    `]
+})
+export class WarningComponent {
+
+}
